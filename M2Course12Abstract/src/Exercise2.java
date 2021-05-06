d74148444d3087e6b1b712715913d154928e75ba
vieru camelia <camelia.vieru31@gmail.com>
2021-05-06 12:48:03 +0300

    tema 12

---
+++ M2Course12Abstract/src/Exercise2.java
@@ -0,0 +1,10 @@
+
+public class Exercise2 {
+
+		/*Fara implementarea unor clase suplimentare abstractizarea nu este posibila,
+		 * pentru ca execitiul se axeaza pe compunere si agregare, nu pe mostenire.
+		 * 
+		 *  Ar putea fi folosita abstractizarea in cazul in care am avea o clasa generala
+		 *  Smartphone care extinde alte clase specifice (Samsung, Huawei etc).
+		 */
+}
---
+++ M2Course12Abstract/src/Shape.java
@@ -0,0 +1,50 @@
+
+public abstract class Shape {
+	private String text;
+	private String material;
+	
+	public Shape() {
+	}
+	
+	public Shape(String text, String material) {
+		this.text = text;
+		this.material = material;
+	}
+	
+	public abstract double getSize();
+
+	
+	@Override
+	public String toString() {
+		return "made of " + material +", contains the text: " + text;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		
+		if(obj == null) {
+			return false;
+		}
+		
+		if(this.getClass() != obj.getClass()) {
+			return false;
+		}
+		
+		Shape other = (Shape) obj;
+		
+		if(this.text.equals(other.text) && this.material.equals(other.material)) {
+			return true;
+		}
+		return false;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (text == null ? 0 : text.hashCode());
+		result = prime * result + (text == null ? 0 : material.hashCode());
+		return result;
+	}
+}
