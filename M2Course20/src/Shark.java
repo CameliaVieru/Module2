c89a45f84ccb60c91ecf45675bc3838cbdd5094a
vieru camelia <camelia.vieru31@gmail.com>
2021-05-06 13:07:27 +0300

    tema 20

---
+++ M2Course20/src/Shark.java
@@ -0,0 +1,16 @@
+
+public class Shark extends AquaticAnimal{
+	public Shark(String name) {
+		super(name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// both objects have the same type
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+
+		return super.equals(obj);
+	}
+}
---
+++ M2Course20/src/Tuna.java
@@ -0,0 +1,16 @@
+
+public class Tuna extends AquaticAnimal{
+	public Tuna(String name) {
+		super(name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// both objects have the same type
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+
+		return super.equals(obj);
+	}
+}
